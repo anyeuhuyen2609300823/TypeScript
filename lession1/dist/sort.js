bf6293cadb2edfccc237c70bcda17350869cb7f1
anyeuhuyen2609300823 <108939314+anyeuhuyen2609300823@users.noreply.github.com>
2023-03-15 21:45:46 +0700

    lab1 trong file sort

---
+++ lession1/dist/sort.js
@@ -0,0 +1,67 @@
+// lab 1 
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+// khai báo mảng
+var arr1 = [1, 2, 3, 10, 14, 4, 5];
+// sử dụng selection sort 
+// thử test code , còn lỗi chưa bug được 
+function selectionSort(arr) {
+    var _a;
+    for (var i = 0; i < arr.length - 1; i++) {
+        var minIndex = i;
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            _a = [arr[minIndex], arr[i]], arr[i] = _a[0], arr[minIndex] = _a[1];
+        }
+    }
+    return arr;
+}
+var result_2 = selectionSort(arr1);
+console.log(result_2);
+// sử dụng vòng lặp lồng để sắp xếp theo thứ tự tăng dần
+function sortArray(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+var result_1 = sortArray(arr1);
+console.log(result_1);
+// sử dụng quick sort 
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    var pivot = arr[0];
+    var left = [];
+    var right = [];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        }
+        else {
+            right.push(arr[i]);
+        }
+    }
+    return __spreadArray(__spreadArray(__spreadArray([], quickSort(left), true), [pivot], false), quickSort(right), true);
+}
+var result_3 = quickSort(arr1);
+console.log(result_3);
---
+++ lession1/src/sort.ts
@@ -0,0 +1,77 @@
+// lab 1 
+
+
+// khai báo mảng
+const arr1 = [1, 2, 3, 10, 14 ,4, 5];
+
+// sử dụng selection sort 
+// thử test code , còn lỗi chưa bug được 
+
+function selectionSort<T>(arr: T[]): T[] {
+    for (let i = 0; i < arr.length - 1; i++) {
+      let minIndex = i;
+  
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+  
+      if (minIndex !== i) {
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+      }
+    }
+  
+    return arr;
+  }
+
+const result_2 = selectionSort(arr1);
+
+console.log(result_2);
+
+
+
+// sử dụng vòng lặp lồng để sắp xếp theo thứ tự tăng dần
+
+function sortArray<T>(arr: T[]): T[] {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    return arr;
+}
+
+const result_1 = sortArray(arr1)
+
+console.log(result_1);
+
+
+// sử dụng quick sort 
+function quickSort<T>(arr: T[]): T[] {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    const pivot = arr[0];
+    const left: T[] = [];
+    const right: T[] = [];
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+  
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}  
+
+const result_3 = quickSort(arr1)
+console.log(result_3);
